3be81b8397d8b5f458453060bee1fa8b35140221
lhutchinson22 <lbhutchinson022@gmail.com>
2020-10-12 13:54:20 -0700

    prompts added, getting on the right track

--- script.js
+++ script.js
@@ -7,21 +7,28 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  // the lenth of password uder chooses
+  var length = passwordLengthChoice;
+
+  //the compilation of all the answers chosen
+  var userPromptOptions =
+    charTypeLower + charTypeUpper + charTypeNums + charTypeSpecial;
+
+  //WHEN prompted for the length of the password
+  var passwordLengthChoice = prompt("How long would you like your password?");
+
+  //WHEN prompted for character types to include in the password
+  var charTypeLower = prompt("Lowercase?");
+  var charTypeUpper = prompt("Uppercase?");
+  var charTypeNums = prompt("Numbers?");
+  var charTypeSpecial = prompt("Special Characters?");
+
+  for (var i = 0, n = passwordLengthChoice.length; i < length; i++) {
+    password += userPromptOptions.charAt(Math.floor(Math.random() * n));
+  }
+  return password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-//////Beginning of my own code
-//generate random password
-function generatePassword() {
-  var length = 8,
-    characterOptions =
-      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-    passwordFinal = "";
-  for (var i = 0, n = characterOptions.length; i < length; i++) {
-    passwordFinal += characterOptions.charAt(Math.floor(Math.random() * n));
-  }
-  return passwordFinal;
-  //console.log(passwordFinal);
-}
