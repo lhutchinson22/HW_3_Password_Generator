aea9c3a8fc981ca281fd68c457582e9d110f62cf
lhutchinson22 <lbhutchinson022@gmail.com>
2020-10-12 13:27:49 -0700

    generated random passward and displayed in box

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//////Beginning of my own code
+//generate random password
+function generatePassword() {
+  var length = 8,
+    characterOptions =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    passwordFinal = "";
+  for (var i = 0, n = characterOptions.length; i < length; i++) {
+    passwordFinal += characterOptions.charAt(Math.floor(Math.random() * n));
+  }
+  return passwordFinal;
+  //console.log(passwordFinal);
+}
