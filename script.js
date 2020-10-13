9b8a8be40938c69fc9fb0ba2bb0a772ac23069c8
lhutchinson22 <lbhutchinson022@gmail.com>
2020-10-13 08:57:32 -0700

    creating for loop to push to array

--- script.js
+++ script.js
@@ -7,32 +7,70 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
-  function generatePassword() {
-    // the lenth of password user chooses
-    var length = passwordLengthChoice;
+function generatePassword() {
+  var optionsLower = "abcde";
+  var optionsLowerArray = optionsLower.split("");
+  console.log(optionsLowerArray);
 
-    //WHEN prompted for the length of the password
-    var passwordLengthChoice = prompt("How long would you like your password?");
+  var optionsUpper = "ABCDE";
+  var optionsUpperArray = optionsUpper.split("");
 
+  var optionsNum = "1234567890";
+  var optionsNumArray = optionsNum.split("");
+
+  var optionsSpecial = "%^&*(()";
+  var optionsSpecialArray = optionsSpecial.split("");
+
+  var emptyArray = [];
+  var result = "";
+
+  //WHEN prompted for the length of the password
+  var passwordLengthChoice = prompt("How long would you like your password?");
+
+  // conditional statement if length meets criteria
+  if (passwordLengthChoice < 8 || passwordLengthChoice > 128) {
+    generatePassword();
+  } else {
     //WHEN prompted for character types to include in the password
-    var charTypeLower = prompt("Lowercase?");
-    var charTypeUpper = prompt("Uppercase?");
-    var charTypeNums = prompt("Numbers?");
-    var charTypeSpecial = prompt("Special Characters?");
+    var charTypeLower = confirm("Would you like to include Lowercase?");
+    var charTypeUpper = confirm("Uppercase?");
+    var charTypeNums = confirm("Numbers?");
+    var charTypeSpecial = confirm("Special Characters?");
 
-    //the compilation of all the answers chosen
-    var userPromptOptions =
-      charTypeLower + charTypeUpper + charTypeNums + charTypeSpecial;
-    console.log(userPromptOptions);
-
-    // a for loop to take in the length of the password and generate a random string
-    for (var i = 0, n = passwordLengthChoice.length; i < length; i++) {
-      password += userPromptOptions.charAt(Math.floor(Math.random() * n));
+    // pushing user character options to empty array to be randomly generated
+    if (charTypeLower) {
+      Array.prototype.push.apply(emptyArray, optionsLowerArray);
     }
-    return password;
+    if (charTypeUpper) {
+      Array.prototype.push.apply(emptyArray, optionsUpperArray);
+    }
+    if (charTypeNums) {
+      Array.prototype.push.apply(emptyArray, optionsNumArray);
+    }
+    if (charTypeSpecial) {
+      Array.prototype.push.apply(emptyArray, optionsSpecialArray);
+    }
+    console.log(emptyArray);
+    // a for loop to take in the length of the password and generate a random string
+    for (var i = 0; i < passwordLengthChoice.length; i++) {
+      //return the index of the random character in the emptyArray
+      var randomIndex = Math.floor(Math.random() * emptyArray.length);
+      //return the actual character
+      result += emptyArray[randomIndex];
+      //emptyArray[0]
+      console.log(randomIndex);
+    }
+    console.log(result);
+    console.log(emptyArray);
+
+    //return password;
+
+    //returning undefined
   }
+  console.log(result);
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
