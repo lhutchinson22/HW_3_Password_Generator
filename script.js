3af0a92575b95974811971c374aa6298ab23512c
lhutchinson22 <lbhutchinson022@gmail.com>
2020-10-13 18:13:01 -0700

    fixed for loopprinted random password on to the html

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  console.log(password);
 
   passwordText.value = password;
 }
 
 function generatePassword() {
-  var optionsLower = "abcde";
+  var optionsLower = "abcdefghijklmnopqrstuvwxyz";
   var optionsLowerArray = optionsLower.split("");
-  console.log(optionsLowerArray);
 
-  var optionsUpper = "ABCDE";
+  var optionsUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var optionsUpperArray = optionsUpper.split("");
 
   var optionsNum = "1234567890";
   var optionsNumArray = optionsNum.split("");
 
-  var optionsSpecial = "%^&*(()";
+  var optionsSpecial = "`!@#$%^&&*()?/`";
   var optionsSpecialArray = optionsSpecial.split("");
 
   var emptyArray = [];
@@ -29,15 +29,15 @@ function generatePassword() {
   //WHEN prompted for the length of the password
   var passwordLengthChoice = prompt("How long would you like your password?");
 
-  // conditional statement if length meets criteria
+  // conditional statement if length meets criteria, if not, then ask again
   if (passwordLengthChoice < 8 || passwordLengthChoice > 128) {
     generatePassword();
   } else {
     //WHEN prompted for character types to include in the password
     var charTypeLower = confirm("Would you like to include Lowercase?");
-    var charTypeUpper = confirm("Uppercase?");
-    var charTypeNums = confirm("Numbers?");
-    var charTypeSpecial = confirm("Special Characters?");
+    var charTypeUpper = confirm("Would you like to include Uppercase??");
+    var charTypeNums = confirm("Would you like to include Numbers??");
+    var charTypeSpecial = confirm("Include Special Characters?");
 
     // pushing user character options to empty array to be randomly generated
     if (charTypeLower) {
@@ -52,25 +52,21 @@ function generatePassword() {
     if (charTypeSpecial) {
       Array.prototype.push.apply(emptyArray, optionsSpecialArray);
     }
-    console.log(emptyArray);
+
     // a for loop to take in the length of the password and generate a random string
-    for (var i = 0; i < passwordLengthChoice.length; i++) {
+    for (var i = 0; i < passwordLengthChoice; i++) {
       //return the index of the random character in the emptyArray
       var randomIndex = Math.floor(Math.random() * emptyArray.length);
       //return the actual character
       result += emptyArray[randomIndex];
       //emptyArray[0]
-      console.log(randomIndex);
+      //   console.log("index of the choices being push to array: " + randomIndex);
     }
-    console.log(result);
-    console.log(emptyArray);
-
-    //return password;
-
-    //returning undefined
+    console.log("password length choice: " + passwordLengthChoice);
+    console.log("actual values being pushed to empty array: " + result);
   }
-  console.log(result);
+  return result;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+generateBtn.addEventListener("click", writePassword);
